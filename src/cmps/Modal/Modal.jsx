d51dd8899b9bcd88ce1fb7d322991b4d7140a32f
bar varfman <barrestyle@gmail.com>
2020-11-03 11:53:34 +0200

    sdf

---
+++ src/cmps/Modal/Modal.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { updateIsModalOpen } from '../../actions/modalAction.js';
+import StoreService from '../../services/StoreService';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import './Modal.scss';
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function _Modal(props) {
+    const location = useLocation()
+
+    const handleClose = () => {
+        props.updateIsModalOpen(false)
+        if(location.pathname === '/form'||location.pathname === '/cancelAppointment') StoreService.initApp()
+        if(location.pathname === '/form')  props.history.push('/treatments')
+    }
+
+    return (
+        <div>
+            <Dialog
+                open={props.isModalOpen}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-slide-title"
+                aria-describedby="alert-dialog-slide-description"
+          >
+                {props.modalTitle && <DialogTitle id="alert-dialog-slide-title">{props.modalTitle}</DialogTitle>}
+                <DialogContent
+                    style={{width:'300px', textAlign:'center'}}
+                >
+                    {props.modalContent && <DialogContentText id="alert-dialog-slide-description">
+                        {props.modalContent}
+                    </DialogContentText>}
+                </DialogContent>
+                <DialogActions>
+                    {/* <Button onClick={handleClose} color="primary">
+                        ביטול
+              </Button> */}
+                    <Button onClick={handleClose} color="primary">
+                        אישור
+             </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+
+
+
+function mapStateProps(state) {
+    return {
+        isModalOpen: state.ModalReducer.isModalOpen
+    }
+}
+
+const mapDispatchToProps = {
+    updateIsModalOpen
+}
+
+export const Modal = withRouter(connect(mapStateProps, mapDispatchToProps)(_Modal))
